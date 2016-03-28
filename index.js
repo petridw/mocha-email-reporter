710232808d7a5ee4194950bd0c305f1e435843ef
davidpetri <petridw@gmail.com>
2016-03-28 11:41:41 -0700

    (feat) fix error if beforeHook fails

--- index.js
+++ index.js
@@ -13,9 +13,9 @@ const emailTemplate = ejs.compile(fs.readFileSync(`${__dirname}/lib/layout.ejs`,
 function EmailReporter(runner, options) {
   var self = this;
   Base.call(self, runner);
-  
+
   self.reporterOptions = options && options.reporterOptions ? options.reporterOptions : {};
-  
+
   self.result = {
     suites: {},
     totalPasses: 0,
@@ -24,14 +24,14 @@ function EmailReporter(runner, options) {
 
   runner.on('suite', (suite) => {
     var title = suite.title;
-    if (!title) return;
-    
+    if (!title) title = 'blankTitle';
+
     self.result.suites[title] = {
       file: suite.file,
       tests: {}
     };
   });
-  
+
   runner.on('test', (test) => {
     self.result.suites[test.parent.title].tests[test.title] = {
       start: Date.now()
@@ -39,26 +39,34 @@ function EmailReporter(runner, options) {
     // self.result.suites[test.parent.title][tests][test.title].start = Date.now();
     // self.result.suites[test.parent.title][test.title].file = test.file;
   });
-  
+
   runner.on('test end', (test) => {
     var start = self.result.suites[test.parent.title].tests[test.title].start;
     self.result.suites[test.parent.title].tests[test.title].duration = Date.now() - start;
     if (test.err) self.result.suites[test.parent.title].tests[test.title].err = test.err;
   });
-  
+
   runner.on('pass', (test) => {
     self.result.suites[test.parent.title].tests[test.title].pass = true;
     self.result.totalPasses += 1;
   });
 
   runner.on('fail', (test, err) => {
-    self.result.suites[test.parent.title].tests[test.title].pass = false;
+    const suite = self.result.suites[test.parent.title];
+    if (suite.tests[test.title]) {
+      suite.tests[test.title].pass = false;
+    } else {
+      // failed early
+      suite.tests.beforeHook = {};
+      suite.tests.beforeHook.pass = false;
+    }
+
     self.result.totalFailures += 1;
   });
 
   runner.on('end', () => {
     if (self.reporterOptions.json) fs.writeFileSync(self.reporterOptions.json, JSON.stringify(self.result, null, 2));
-    
+
     process.stdout.write(emailTemplate({
       results: self.result,
       headerContent: self.reporterOptions.header || '',
